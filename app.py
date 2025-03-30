4d25482f1d986d538d63b68956c4235c3b38dee9
Stiveys <stevomwaura@gmail.com>
2025-03-30 10:41:57 +0300

    new

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from extensions import db, migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db.init_app(app)
+migrate.init_app(app, db)
 
 from models import Hero, Power, HeroPower
 
